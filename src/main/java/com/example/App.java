d09dbac5f0621559296f8906d96aaf927dafe85d
lucasferreira.izap <lucasrodrigues.lrf@gmail.com>
2023-08-31 18:11:24 -0300

    atualizacao projeto demo

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,35 +1,44 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main(String[] args)
-    {                        
+public class App {
+    public static void main(String[] args) {
         TiposVeiculosEnum tiposVeiculosEnum = TiposVeiculosEnum.CARRO;
-        System.out.println("NUMERO_RODAS: "+tiposVeiculosEnum.getNumeroRodas());
+        System.out.println("NUMERO_RODAS: " + tiposVeiculosEnum.getNumeroRodas());        
 
         ErrosDoSistemaEnum errosDoSistemaEnum = ErrosDoSistemaEnum.EXCEPTION_ACESSO_BANCO;
-        
-        try {
-            throw new Throwable(errosDoSistemaEnum.getMensagem());
-        } catch (Throwable e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+
+        /* Exemplo de estrutura de dados em java */
+        Integer testeString[] = new Integer[50];
+        testeString[0] = 1;
+
+        List<String> list = new ArrayList<String>();
+        list.add("TESTE");
+        list.add("TESTE");
+        list.add("TESTE");
+        list.add("TESTE");
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("MeuAtributo", 0);
     }
-   
+
     public void funcaoTeste(Short teste) {
         // regra de negocio z
     }
 
-    public void funcaoTeste(Integer teste){
-        // regra de negocio y     
+    public void funcaoTeste(Integer teste) {
+        // regra de negocio y
     }
 
-    public void funcaoTeste(){
+    public void funcaoTeste() {
         // regra de negocio x
     }
 }
